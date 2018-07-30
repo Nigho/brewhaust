601f28877220f64a9ab42771fb5bbb50ff4c6e91
Nico Lucciola <nico.lucciola@gmail.com>
2018-07-30 15:46:13 +0200

    adjust units

--- src/qt/bitcoinunits.h
+++ src/qt/bitcoinunits.h
@@ -59,9 +59,9 @@ public:
       @note Source: https://en.bitcoin.it/wiki/Units . Please add only sensible ones
      */
     enum Unit {
-        BREWHAUST,
-        mBREWHAUST,
-        uBREWHAUST
+        BRU,
+        mBRU,
+        uBRU
     };
 
     enum SeparatorStyle {
